3e83932aac16b7eb8babac2321d5f5b522f75ab0
eman1970 <tommy.hallqvist@gmail.com>
2021-01-20 20:20:36 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Greeting } from './components/greeting/Greeting'
+
+
+function App() {
+  return (
+    <>
+      
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/greeting/Greeting.tsx
@@ -0,0 +1,20 @@
+import './Greeting.css'
+
+export const Greeting = () => {
+
+
+return (
+
+    <>
+
+<h1 className="title">Hello! Awesome website ok!</h1>
+<h1>Hello! Awesome website ok!</h1>
+
+</>
+
+)
+
+
+
+}
+
