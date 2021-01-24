0c429a9004f0bb272cdc5ec6ef4191105f7febef
eman1970 <tommy.hallqvist@gmail.com>
2021-01-24 01:18:38 +0100

    Last

--- src/routes/RoutingPath.ts
+++ src/routes/RoutingPath.ts
@@ -1,12 +1,14 @@
 const homeView = '/'
 const aboutView = '/about'
 const signInView = '/signin'
+const contactView = '/contact'
 
 export default {
 
 homeView,
 aboutView,
-signInView
+signInView,
+contactView
 
 
 }
