4d784dbe8fdfd6f97a3594e44d3c52199ce79926
eman1970 <tommy.hallqvist@gmail.com>
2021-01-26 21:44:48 +0100

    Dropdown menu

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -8,17 +8,20 @@ export const Profile = () => {
     const [authUser, setAuthUser] = useContext(UserContext)
     return (
         <div className="profileWrapper">
-        <div>
-            <img className="profileImg" src={'https://thispersondoesnotexist.com/image'} />
-            {authUser.username}
-            <div className="profileDropdown">
-            <span>alternativ 1</span>
-            <span>alternativ 2</span>
-            <span>alternativ 3</span>
-            <span>alternativ 4</span>
-            </div>
 
-        </div>
-        </div>
+            <img className="profileImg" src={'https://thispersondoesnotexist.com/image'} />
+            { authUser.username}
+            <div className="profileDropdown">
+
+                <div className="dropdown-content">
+                    <span><a href="#">Mouse over me</a></span><br />
+                    <span><a href="#">Mouse over me</a></span><br />
+                    <span><a href="#">Mouse over me</a></span><br />
+                    <span><a href="#">Mouse over me</a></span>
+                </div>
+            </div>
+        </div >
+
+
     )
 }
